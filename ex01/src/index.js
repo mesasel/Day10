83d2ef5c0c9a4b4914b81352c12bcb9f05565eb9
Meša Selimović <mesa_sel@hotmail.com>
2021-03-17 19:17:13 +0100

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+
+var myPet = {
+  species: "Retriver",
+  name: "Gonzo",
+  legs: 4,
+  friends: ["Šaro", "Garo"],
+};
+
+function myFunction(myObj) {
+  return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
